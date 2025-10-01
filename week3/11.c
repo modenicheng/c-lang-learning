6dbdefb3859fe5365c19d9a27a1b8367cbb47733
modenicheng <cheng20070225@outlook.com>
2025-10-01 09:27:08 +0800

    fix: 在分针走的时候，时针也要走 : (

--- week3/11.c
+++ week3/11.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float abs(float num)
+double abs(double num)
 {
     return num < 0 ? -num : num;
 }
@@ -9,16 +9,9 @@ int main()
 {
     int h, m;
     scanf("%d%d", &h, &m);
-    float h_degree = 360.0 / 12 * h;
-    while (h_degree >= 360)
-    {
-        h_degree -= 360;
-    }
-    float m_degree = 360.0 / 60 * m;
-    while (m_degree >= 360) {
-        m_degree -= 360;
-    }
-    float d = abs(h_degree - m_degree);
-    printf("At %.2d:%.2d the angle is %.1f degrees.\n", h, m, d > 180 ? d - 180 : d);
+    double h_degree = 30 * h + 0.5 * m;
+    double m_degree = 6 * m;
+    double d = abs(h_degree - m_degree);
+    printf("At %d:%.2d the angle is %.1f degrees.\n", h, m, d > 180 ? d - 180 : d);
     return 0;
 }
