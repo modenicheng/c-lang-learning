862b8f8b98c16469983304e1f43897c02b65529e
modenicheng <cheng20070225@outlook.com>
2025-11-03 16:16:01 +0800

    Add EXP1.6.c

---
+++ week8/EXP1.6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(int argc, char *argv[])
+{
+	int n, num;
+	scanf("%d%d", &n, &num);
+	int i, j;
+	 
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < 3 * n - 2 * i - 2; j++)
+		{
+			if ((i == 0 || i == n-1) || (j == 0 || j == 3 * n - 2 * i - 3)) {
+				if ((i + j + 1) * 2 > (3 * n - 2))
+				{
+					printf("%d", (num + i + (3 * n - 2 * i - 2) - j - 1) % 10);
+//					printf("*");
+				} else {
+					printf("%d", (num + i + j) % 10);
+				}
+				
+			} else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
+
