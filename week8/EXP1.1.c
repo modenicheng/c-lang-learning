4ab786406c25c30d69cb1ba99e793c647c5de8b6
modenicheng <cheng20070225@outlook.com>
2025-11-03 16:13:21 +0800

    Add  EXP1.1

---
+++ week8/EXP1.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int n;
+	scanf("%d", &n);
+	
+	int i, j;
+	for (i = n; i > 0; i--)
+	{
+		for (j = 0; j < n - i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < 2 * i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
